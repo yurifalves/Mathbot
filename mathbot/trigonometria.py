93efb368609516aea47b5f518cec2cd97915f7e7
Yuri <87913263+yurifalves@users.noreply.github.com>
2022-04-05 06:32:47 -0300

    Update trigonometria.py

--- mathbot/trigonometria.py
+++ mathbot/trigonometria.py
@@ -23,7 +23,7 @@ def trig(angulo):
     seno, cos, tg = math.sin(ang), math.cos(ang), math.tan(ang)
     ang_congruo = angulo
     result = f"""
-            Considerando o ângulo {angulo:.2f}°
+            Considerando o ângulo {angulo}°:
         Sen({angulo}°) = {seno:.2f}
         Cos({angulo}°) = {cos:.2f}
         Tg({angulo}°) = {tg:.2f}
