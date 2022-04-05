efb95a0761682f3d4ab45094c8c58adbc81cdbeb
Yuri <87913263+yurifalves@users.noreply.github.com>
2022-04-05 06:33:49 -0300

    Update primos.py

--- mathbot/primos.py
+++ mathbot/primos.py
@@ -10,16 +10,22 @@ def checarintervalo(a, b, chatid):
     from bob_telegram_tools.utils import TelegramTqdm
     from bob_telegram_tools.bot import TelegramBot
     if a < 0 or a > b:
-        return f'alguma condição está sendo violada: a < 0 ou a < b'
+        return f'Alguma condição está sendo violada: a < 0 ou a < b'
     else:
-        listaprimos = []
         bot_bob = TelegramBot('5232695368:AAH_CQAYVlI0TVEas8XSppXvBOA1wY1LjY4', chatid)
         pb = TelegramTqdm(bot_bob)
-        for n in pb(range(a, b + 1)):
-            cont = 0
-            for t in range(1, n + 1):
-                if n % t == 0:
-                    cont += 1
-            if cont == 2:
-                listaprimos.append(n)
+        
+        def is_prime(n):
+            if n == 2 or n == 3: return True
+            if n < 2 or n % 2 == 0: return False
+            if n < 9: return True
+            if n % 3 == 0: return False
+            r = int(n ** 0.5)
+            f = 5
+            while f <= r:
+                if n % f == 0: return False
+                if n % (f + 2) == 0: return False
+                f += 6
+            return True
+        listaprimos = [n for n in pb(range(a, b + 1)) if is_prime(n)]
         return f'Considerando o intervalo [{a}, {b}]\n\n-NÚMEROS PRIMOS: {listaprimos}'
