308548b9934efa5c4ab68a0fe4d5fe82b0591f5b
Yuri <87913263+yurifalves@users.noreply.github.com>
2022-04-13 05:59:49 -0300

    Create currencyapi.py

---
+++ cotacoesbot/currencyapi.py
@@ -0,0 +1,35 @@
+def currencyapi(KEY):
+    """
+    Currencyapi --> https://app.currencyapi.com/
+    :param KEY: API Key.
+    :return: String com as cotações de todas as moedas disponíveis na API.
+    """
+    import requests
+    res = requests.get(f'https://api.currencyapi.com/v3/latest?apikey={KEY}&base_currency=BRL').json()
+    last_updated = res['meta']['last_updated_at']
+    moedas = [moeda for moeda in res['data']]
+    quantidade_moedas = len([moeda for moeda in res['data']])
+    texto1 = f'{quantidade_moedas} Moedas\n({last_updated})\n\n'  # Moedas importantes
+    texto2 = str()  # Moedas Secundárias
+
+    moedas_importantes = {'USD': 'Dólar Americano',
+                          'EUR': 'Euro',
+                          'GPB': 'Libra esterlina',
+                          'JPY': 'Iene japonês',
+                          'CHF': 'Franco suíço',
+                          'CAD': 'Dólar canadense',
+                          'AUD': 'Dólar Australiano',
+                          'RUB': 'Rublo russo',
+                          'ARS': 'Peso argentino'}
+
+    c = 0
+    while c < quantidade_moedas:
+        moeda = moedas[c]
+        code, valor = res['data'][moeda]['code'], str(round(1/res['data'][moeda]['value'], 6)).replace('.', ',')
+        if code in moedas_importantes:
+            texto1 += f'1 {code} ({moedas_importantes[code]}) = R$ {valor}\n'
+        else:
+            texto2 += f'1 {code} = R$ {valor}\n'
+        c += 1
+    return texto1 + texto2
+  
