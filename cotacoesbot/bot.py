118cd2496bab8712bec25e2f4b3d859f101acbcb
Yuri <87913263+yurifalves@users.noreply.github.com>
2022-04-13 06:06:07 -0300

    Update bot.py

--- cotacoesbot/bot.py
+++ cotacoesbot/bot.py
@@ -1,1 +1,29 @@
-pass
+import telebot
+from telebot import types
+
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(func=lambda mensagem: True if mensagem.text == 'currencyapi' else False)
+@bot.message_handler(commands=['currencyapi'])
+def currencyapi(mensagem):
+    markup = types.ReplyKeyboardRemove(selective=False)
+    bot.send_message(mensagem.chat.id, currencyapi.currencyapi(KEY), parse_mode='Markdown', reply_markup=markup)
+
+
+@bot.message_handler(func=lambda mensagem: True)
+def responder(mensagem):
+    texto_padrao = """
+    *MENU INICIAL*
+    /currencyapi
+    /informacoes
+    """
+    markup = types.ReplyKeyboardMarkup()
+    itembtn1 = types.KeyboardButton('currencyapi')
+    itembtn2 = types.KeyboardButton('informações')
+    markup.row(itembtn1)
+    markup.row(itembtn2)
+    bot.send_message(mensagem.chat.id, texto_padrao, parse_mode='Markdown', reply_markup=markup)
+
+
+bot.polling()
