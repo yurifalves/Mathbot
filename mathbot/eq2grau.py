6a2d2537492095a3fafce0436250f85470455274
Yuri <87913263+yurifalves@users.noreply.github.com>
2022-04-27 04:51:14 -0300

    Update eq2grau.py

--- mathbot/eq2grau.py
+++ mathbot/eq2grau.py
@@ -1,19 +1,18 @@
 def eq2grau(a, b, c):
-    import math
-    discriminante = b ** 2 - 4 * a * c
-    if discriminante >= 0:
-        x1, x2 = (-b + math.sqrt(discriminante)) / (2 * a), (-b - math.sqrt(discriminante)) / (2 * a)
-        return f'X1 = {x1:.2f}\nX2 = {x2:.2f}'
+    from cmath import sqrt
+    equacao = f'{a}x²+{b}x+{c}'
+    discriminante = b**2 - 4*a*c
+    x1, x2 = (-b + sqrt(discriminante))/2, (-b - sqrt(discriminante))/2
+    if x1.imag == 0:
+        x1 = x1.real
     else:
-        discriminante = abs(discriminante)
-        x1_a, x1_b = -b / (2 * a), math.sqrt(discriminante) / (2 * a)
-        x2_a, x2_b = -b / (2 * a), -math.sqrt(discriminante) / (2 * a)
-        if x1_b < 0:
-            x1 = f'{x1_a:.2f} - {abs(x1_b):.2f}i'
-        else:
-            x1 = f'{x1_a:.2f} + {x1_b:.2f}i'
-        if x2_b < 0:
-            x2 = f'{x2_a:.2f} - {abs(x2_b):.2f}i'
-        else:
-            x2 = f'{x2_a:.2f} + {x2_b:.2f}i'
-        return f'X1 = {x1}\nX2 = {x2}'
+        x1 = f'{x1.real} + {x1.imag}i'
+    if x2.imag == 0:
+        x2 = x2.real
+    else:
+        x2 = f'{x2.real} + {x2.imag}i'
+    return f'{equacao}\nx1 = {x1}, x2 = {x2}'
+
+
+if __name__ == '__main__':
+    print(eq2grau(1, -3, -4))
