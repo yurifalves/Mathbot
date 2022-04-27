48c1728c8bb5ab90576eaff58868024773643ae3
Yuri <87913263+yurifalves@users.noreply.github.com>
2022-04-27 04:56:11 -0300

    Update eq2grau.py

--- mathbot/eq2grau.py
+++ mathbot/eq2grau.py
@@ -6,13 +6,19 @@ def eq2grau(a, b, c):
     if x1.imag == 0:
         x1 = x1.real
     else:
-        x1 = f'{x1.real}+{x1.imag}i'
+        if '-' in str(x1.imag):
+            x1 = f'{x1.real}{x1.imag}i'
+        else:
+            x1 = f'{x1.real}+{x1.imag}i'
     if x2.imag == 0:
         x2 = x2.real
     else:
-        x2 = f'{x2.real}+{x2.imag}i'
+        if '-' in str(x2.imag):
+            x2 = f'{x2.real}{x2.imag}i'
+        else:
+            x2 = f'{x2.real}+{x2.imag}i'
     return f'{equacao}\nx1 = {x1}, x2 = {x2}'
 
 
 if __name__ == '__main__':
-    print(eq2grau(1, -3, -4))
+    print(eq2grau(4, -4, 2))
