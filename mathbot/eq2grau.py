d17e36b1882ef846a56f5400daa2bf93d45dbe1b
Yuri <87913263+yurifalves@users.noreply.github.com>
2022-05-01 18:46:50 -0300

    Update eq2grau.py

--- mathbot/eq2grau.py
+++ mathbot/eq2grau.py
@@ -1,22 +1,26 @@
 def eq2grau(a, b, c):
     from cmath import sqrt
-    equacao = f'{a}x²+{b}x+{c}'
+
     discriminante = b**2 - 4*a*c
     x1, x2 = (-b + sqrt(discriminante))/2, (-b - sqrt(discriminante))/2
+    if '-' in str(b) and '-' in str(c):
+        equacao = f'{a}x²{b}x{c}'
+    if '-' in str(b) and '-' not in str(c):
+        equacao = f'{a}x²{b}x+{c}'
+    if '-' not in str(b) and '-' not in str(c):
+        equacao = f'{a}x²+{b}x+{c}'
+    if '-' not in str(b) and '-' in str(c):
+        equacao = f'{a}x²+{b}x{c}'
+
     if x1.imag == 0:
         x1 = x1.real
     else:
-        if '-' in str(x1.imag):
-            x1 = f'{x1.real}{x1.imag}i'
-        else:
-            x1 = f'{x1.real}+{x1.imag}i'
+        x1 = str(x1).replace('(', '').replace(')', '').replace('j', 'i')
     if x2.imag == 0:
         x2 = x2.real
     else:
-        if '-' in str(x2.imag):
-            x2 = f'{x2.real}{x2.imag}i'
-        else:
-            x2 = f'{x2.real}+{x2.imag}i'
+        x2 = str(x2).replace('(', '').replace(')', '').replace('j', 'i')
+
     return f'{equacao}\nx1 = {x1}, x2 = {x2}'
 
 
