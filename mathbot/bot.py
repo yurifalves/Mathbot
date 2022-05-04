471a05efcb5530eb7d2535a4e96a8de3e7678fdf
Yuri <87913263+yurifalves@users.noreply.github.com>
2022-05-03 21:30:40 -0300

    Create bot.py

---
+++ mathbot/bot.py
@@ -0,0 +1,13 @@
+class Bot:
+
+    @staticmethod
+    def token():
+        telegramtoken = '5232695368:AAH_CQAYVlI0TVEas8XSppXvBOA1wY1LjY4'  # TOKEN do bot
+        return telegramtoken
+
+    @staticmethod
+    def executar():
+        import config
+
+
+Bot.executar()
