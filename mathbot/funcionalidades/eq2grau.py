8a330416c47aac6f8d8f58fdd13d6f2bf9ba3780
Yuri <87913263+yurifalves@users.noreply.github.com>
2022-05-12 23:44:27 -0300

    Update eq2grau.py

--- mathbot/funcionalidades/eq2grau.py
+++ mathbot/funcionalidades/eq2grau.py
@@ -1,8 +1,6 @@
 def eq2grau(a, b, c):
     from cmath import sqrt
 
-    discriminante = b**2 - 4*a*c
-    x1, x2 = (-b + sqrt(discriminante))/2, (-b - sqrt(discriminante))/2
     if '-' in str(b) and '-' in str(c):
         equacao = f'{a}x²{b}x{c}'
     if '-' in str(b) and '-' not in str(c):
@@ -12,17 +10,18 @@ def eq2grau(a, b, c):
     if '-' not in str(b) and '-' in str(c):
         equacao = f'{a}x²+{b}x{c}'
 
-    if x1.imag == 0:
-        x1 = x1.real
+    if a == 0:
+        x1 = x2 = -c/b
+        x1 = str(x1).replace('.', ',')
+        x2 = str(x2).replace('.', ',')
     else:
-        x1 = str(x1).replace('(', '').replace(')', '').replace('j', 'i')
-    if x2.imag == 0:
-        x2 = x2.real
-    else:
-        x2 = str(x2).replace('(', '').replace(')', '').replace('j', 'i')
+        discriminante = b ** 2 - 4*a*c
+        x1, x2 = (-b + sqrt(discriminante)) / (2*a), (-b - sqrt(discriminante)) / (2*a)
+        x1 = str(x1).replace('(', '').replace(')', '').replace('.', ',').replace('j', 'i')
+        x2 = str(x2).replace('(', '').replace(')', '').replace('.', ',').replace('j', 'i')
 
-    return f'{equacao}\nx1 = {x1}, x2 = {x2}'
+    return f'{equacao}\nx₁ = {x1}\nx₂ = {x2}'
 
 
 if __name__ == '__main__':
-    print(eq2grau(4, -4, 2))
+    print(eq2grau(4, 4, 2))
