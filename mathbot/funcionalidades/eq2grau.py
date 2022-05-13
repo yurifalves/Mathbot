c365beb2fbb9a7a3e0ea0db3f4e1cd32c91df4a9
Yuri <87913263+yurifalves@users.noreply.github.com>
2022-05-12 23:59:12 -0300

    Update eq2grau.py

--- mathbot/funcionalidades/eq2grau.py
+++ mathbot/funcionalidades/eq2grau.py
@@ -1,14 +1,15 @@
 def eq2grau(a, b, c):
     from cmath import sqrt
+    a_eq, b_eq, c_eq = str(a).replace('.', ','), str(b).replace('.', ','), str(c).replace('.', ',')
 
     if '-' in str(b) and '-' in str(c):
-        equacao = f'{a}x²{b}x{c}'
+        equacao = f'{a_eq}x²{b_eq}x{c_eq}'
     if '-' in str(b) and '-' not in str(c):
-        equacao = f'{a}x²{b}x+{c}'
+        equacao = f'{a_eq}x²{b_eq}x+{c_eq}'
     if '-' not in str(b) and '-' not in str(c):
-        equacao = f'{a}x²+{b}x+{c}'
+        equacao = f'{a_eq}x²+{b_eq}x+{c_eq}'
     if '-' not in str(b) and '-' in str(c):
-        equacao = f'{a}x²+{b}x{c}'
+        equacao = f'{a_eq}x²+{b_eq}x{c_eq}'
 
     if a == 0:
         x1 = x2 = -c/b
@@ -24,4 +25,4 @@ def eq2grau(a, b, c):
 
 
 if __name__ == '__main__':
-    print(eq2grau(4, 4, 2))
+    print(eq2grau(4.43294832, 4.4329432, 2.432423))
