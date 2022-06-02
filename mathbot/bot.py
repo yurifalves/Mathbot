c03c8f6289a20a6f89eea82518527ae4e429ce9e
Yuri <87913263+yurifalves@users.noreply.github.com>
2022-06-02 02:43:01 -0300

    Update bot.py

--- mathbot/bot.py
+++ mathbot/bot.py
@@ -2,8 +2,13 @@ class Bot:
 
     @staticmethod
     def token():
-        telegramtoken = TOKEN  # TOKEN do bot
+        telegramtoken = '522222228:AAH_CQAYVlI0TVzzzzzzzzzzzA1wY1LjY4'  # TOKEN do bot telegram
         return telegramtoken
+
+    @staticmethod
+    def random_api():
+        API_Key = '6aaaaa9-39b1-4c76-bc28-e63aaaaaaa8'  # https://api.random.org/
+        return API_Key
 
     @staticmethod
     def executar():
