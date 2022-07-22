203a1238efaddfde1b8dbfcf1ca18d4a6736a96f
Yuri <87913263+yurifalves@users.noreply.github.com>
2022-07-21 21:40:53 -0300

    Update matrizes.py

--- mathbot/funcionalidades/matrizes.py
+++ mathbot/funcionalidades/matrizes.py
@@ -7,31 +7,33 @@ class Matriz:
 
     def __init__(self, matriz):
         self._matriz = matriz
-        self.tamanho = matriz.shape
+        self.tamanho = f'{matriz.shape[0]}×{matriz.shape[1]}'
+        self.determinante = linalg.det(self._matriz, overwrite_a=True, check_finite=False)
 
     def forma_escalonada_reduzida(self):
         M = sympy.Matrix(self._matriz)
         forma_escalonada_reduzida = np.array(M.rref(pivots=False))
         return forma_escalonada_reduzida
 
-    def inversa(self):
-            matriz_inversa = linalg.inv(self._matriz, overwrite_a=True, check_finite=False)
-            return matriz_inversa
+    def matriz_inversa(self):
+        matriz_inversa = linalg.inv(self._matriz, overwrite_a=True, check_finite=False)
+        return matriz_inversa
 
-    def transposta(self):
+    def matriz_transposta(self):
         return self._matriz.T
 
-    def determinante(self):
-        determinante = linalg.det(self._matriz, overwrite_a=True, check_finite=False)
-        return determinante
-
+    def matriz_adjunta(self):
+        adjunta = self.determinante * self.matriz_inversa()
+        return adjunta
+    def matriz_dos_cofatores(self):
+        cofatores = self.matriz_adjunta().T
+        return cofatores
 
 if __name__ == '__main__':
     A = np.array([
-        [1, 0, 0],
-        [4, 5, 6],
-        [7, 7, 9]
+        [1, 2],
+        [3, 4]
     ])
 
     A = Matriz(A)
-    print(A.forma_escalonada_reduzida())
+    print(A.determinante)
